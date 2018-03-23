6ed1aef1c24c3fe51d9b57cbf9cfe54ad9c70467
Cyy <cyy1079276272@163.com>
2018-03-23 20:42:16 +0800

    init

---
+++ code/TimeSeriesForecasting.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/23 下午8:36
+# @Author  : Cyy
+# @File    : TimeSeriesForecasting.py
+# @Software: PyCharm
+
+# load and plot dataset
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+# load dataset
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+series = read_csv('../data/shampoo.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# summarize first few rows
+print(series.head())
+# line plot
+series.plot()
+pyplot.show()
