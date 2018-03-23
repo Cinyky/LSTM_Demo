2965f58f138fdd3f563e9813227a5ac9727403f4
Cyy <cyy1079276272@163.com>
2018-03-23 20:55:25 +0800

    优化

--- code/TimeSeriesForecasting.py
+++ code/TimeSeriesForecasting.py
@@ -11,6 +11,9 @@
 from pandas import read_csv
 from pandas import datetime
 from matplotlib import pyplot
+from pandas import datetime
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 # load dataset
 def parser(x):
 	return datetime.strptime('190'+x, '%Y-%m')
@@ -19,4 +22,23 @@ series = read_csv('../data/shampoo.csv', header=0, parse_dates=[0], index_col=0,
 print(series.head())
 # line plot
 series.plot()
+pyplot.show()
+
+# split data into train and test
+X = series.values
+train, test = X[0:-12], X[-12:]
+# walk-forward validation
+history = [x for x in train]
+predictions = list()
+for i in range(len(test)):
+	# make prediction
+	predictions.append(history[-1])
+	# observation
+	history.append(test[i])
+# report performance
+rmse = sqrt(mean_squared_error(test, predictions))
+print('RMSE: %.3f' % rmse)
+# line plot of observed vs predicted
+pyplot.plot(test)
+pyplot.plot(predictions)
 pyplot.show()
