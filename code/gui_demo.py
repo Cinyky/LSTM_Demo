21512d96130ec0e4554f5e6ed3aa67141a0d0935
Cyy <cyy1079276272@163.com>
2018-03-26 17:17:51 +0800

    二维码添加明细

---
+++ code/gui_demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/26 下午3:45
+# @Author  : Cyy
+# @File    : gui_demo.py
+# @Software: PyCharm
+
+import wx; a=wx.App(); wx.Frame(None, title="Hello World").Show(); a.MainLoop()
